9edb99ea2a3d9c5ba4b1ef1d372529b861972a4d
Deniz Cakmak <bdeniz.cakmak@std.hku.edu.tr>
2019-10-04 11:22:36 +0300

    Add files via upload

---
+++ NotificationApp/OpeningPageViewController.swift
@@ -0,0 +1,75 @@
+//
+//  OpeningPageViewController.swift
+//  NotificationApp
+//
+//  Created by Deniz MacBook Air on 30.09.2019.
+//  Copyright © 2019 MacBook Air. All rights reserved.
+//
+
+import UIKit
+import SwiftSoup
+
+var text:String = ""
+
+class OpeningPageViewController: UIViewController {
+
+    @IBOutlet weak var lblData: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getData()
+        sendNotification()
+    }
+    
+    func getData(){
+        
+        do {
+            let url = NSURL(string: "https://compe.hku.edu.tr/category/ilanlar/")
+            print(url!)
+            let html = try NSString(contentsOf: url! as URL, encoding: String.Encoding.utf8.rawValue)
+            let document: Document = try SwiftSoup.parse(html as String)
+            if let data: Element = try document.select("div.entry-details h5.entry-title").first() {
+                let description = try data.text()
+                lblData.text = description
+                print(description)
+                text = description
+            }
+        } catch {
+            
+        }
+    }
+    
+    func sendNotification(){
+        // Step 1: Ask for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+        }
+        
+        // Step 2: Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Yeni bir duyuru var ! "
+        content.body = text
+        
+        // Step 3: Create the notification trigger
+        let date = Date().addingTimeInterval(10)
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // Step 4: Create the request
+        
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        // Step 5: Register the request
+        center.add(request) { (error) in
+            
+        }
+    }
+
+
+}
+
